61b7fdca30e30df74e5675634e4f159483ba0401
mmillan18 <2220211035@estudiantesunibague.edu.co>
2024-04-10 14:56:14 -0500

    fix problems

--- src/main/java/Servidor_Videojuego/Servidor_Videojuego/services/ServicioVideojuego.java
+++ src/main/java/Servidor_Videojuego/Servidor_Videojuego/services/ServicioVideojuego.java
@@ -4,8 +4,7 @@ import Servidor_Videojuego.Servidor_Videojuego.model.Videojuego;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
+
 
 public class ServicioVideojuego implements IServicioVideojuego{
     private Videojuego videojuego;
