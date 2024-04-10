15e2584b42355b6530f37d10e9174d3a08625669
mmillan18 <2220211035@estudiantesunibague.edu.co>
2024-04-10 13:48:25 -0500

    Initial commit

---
+++ src/main/java/Servidor_Videojuego/Servidor_Videojuego/ServidorVideojuegoApplication.java
@@ -0,0 +1,13 @@
+package Servidor_Videojuego.Servidor_Videojuego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorVideojuegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorVideojuegoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Servidor_Videojuego/Servidor_Videojuego/ServidorVideojuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package Servidor_Videojuego.Servidor_Videojuego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorVideojuegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
