83a8b734e9834dded33cbc918b93278a49a42703
mmillan18 <2220211035@estudiantesunibague.edu.co>
2024-04-10 14:21:29 -0500

    Created the ErrorMessage class to encapsulate error details

--- src/main/java/Servidor_Videojuego/Servidor_Videojuego/services/ErrorMessage.java
+++ src/main/java/Servidor_Videojuego/Servidor_Videojuego/services/ErrorMessage.java
@@ -1,4 +1,19 @@
 package Servidor_Videojuego.Servidor_Videojuego.services;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ErrorMessage {
+    private String code;
+    private List<Map<String, String>> mensajes;
 }
+
